263bb05452fb6efbab96faab3497725f9e833a46
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2021-11-06 21:29:32 +0200

    Update install.sh

--- usb_gadget/install.sh
+++ usb_gadget/install.sh
@@ -5,4 +5,4 @@ echo "dwc2" | sudo tee -a /etc/modules # loat at boot
 echo "libcomposite" | sudo tee -a /etc/modules
 cp init-usb-gadget /usr/bin/ # USB gadget configFS
 chmod +x /usr/bin/init-usb-gadget
-sed '/^exit 0/i /usr/bin/init-usb-gadget' /etc/rc.local # libcomposite configuration
+sed -i '/^exit 0/i /usr/bin/init-usb-gadget' /etc/rc.local # libcomposite configuration
