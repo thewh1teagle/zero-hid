05e964a0531628905a64e9c78917d64778ebf6fe
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2021-11-06 19:24:43 +0200

    changes

--- usb_gadget/install.sh
+++ usb_gadget/install.sh
@@ -3,6 +3,6 @@
 echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt # upstream driver which can do the OTG host/gadget flip dictated by OTG_SENSE.
 echo "dwc2" | sudo tee -a /etc/modules # loat at boot
 echo "libcomposite" | sudo tee -a /etc/modules
-cp init_usb_gadget /usr/bin/ # USB gadget configFS
-chmod +x /usr/bin/init_usb_gadget
-sed '/^exit 0/i /usr/bin/init_usb_gadget' /etc/rc.local # libcomposite configuration
+cp init-usb-gadget /usr/bin/ # USB gadget configFS
+chmod +x /usr/bin/init-usb-gadget
+sed '/^exit 0/i /usr/bin/init-usb-gadget' /etc/rc.local # libcomposite configuration
