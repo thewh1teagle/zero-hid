d2be1478cbd66a22879850b6d15fe21418466b50
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2021-11-15 03:15:59 +0200

    configured tests

--- zero_hid/Mouse.py
+++ zero_hid/Mouse.py
@@ -1,4 +1,10 @@
 from .hid.mouse import send_mouse_event
+from typing import SupportsInt
+
+
+class RelativeMoveRangeError(Exception):
+    pass
+
 
 class Mouse:
     def __init__(self, dev='/dev/hidg1') -> None:
@@ -13,7 +19,12 @@ class Mouse:
         send_mouse_event(self.dev, 0x2, 0, 0, 0, 0)
     
     def move_relative(self, x, y):
-        assert -127 <= x <= 127
-        assert -127 <= y <= 127
+        """
+        move the mouse in relative mode
+        x,y should be in range of -127 to 127
+        """
+        if not -127 <= x <= 127: 
+            raise RelativeMoveRangeError(f"Value of x: {x} out of range (-127 - 127)")
+        if not -127 <= y <= 127:
+            RelativeMoveRangeError(f"Value of x: {y} out of range (-127 - 127)")
         send_mouse_event(self.dev, 0x0, x, y, 0, 0)
-    
--- zero_hid/hid/mouse.py
+++ zero_hid/hid/mouse.py
@@ -2,11 +2,11 @@ from . import write as hid_write
 
 def send_mouse_event(mouse_path, buttons, relative_x, relative_y,
                      vertical_wheel_delta, horizontal_wheel_delta):
-
-    buf = [0] * 5
-    buf[0] = buttons
-    buf[1] = relative_x & 0xff
-    buf[2] = relative_y & 0xff
-    buf[3] = vertical_wheel_delta & 0xff
-    buf[4] = horizontal_wheel_delta & 0xff
+    buf = [
+        buttons,
+        relative_x & 0xff,
+        relative_y & 0xff,
+        vertical_wheel_delta & 0xff,
+        horizontal_wheel_delta & 0xff
+    ]
     hid_write.write_to_hid_interface(mouse_path, buf)
