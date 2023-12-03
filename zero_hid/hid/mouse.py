8d3b55713b7408d585287d0231621d9410b5f481
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-12-04 01:50:20 +0200

    working absolute position

--- zero_hid/hid/mouse.py
+++ zero_hid/hid/mouse.py
@@ -5,9 +5,10 @@ def send_mouse_event(dev, buttons, relative_x, relative_y,
     buf = [
         buttons,
         relative_x & 0xff,
+        (relative_x >> 8) & 0xff,  # Extract the upper byte of relative_x
         relative_y & 0xff,
+        (relative_y >> 8) & 0xff,  # Extract the upper byte of relative_y
         vertical_wheel_delta & 0xff,
         horizontal_wheel_delta & 0xff
     ]
     hid_write.write_to_hid_interface(dev, buf)
-    
