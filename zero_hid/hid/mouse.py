09e52db3e64f39a8171a56667334920c60a88933
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-12-04 03:37:57 +0200

    dynamic mouse event

--- zero_hid/hid/mouse.py
+++ zero_hid/hid/mouse.py
@@ -1,24 +1,26 @@
 from . import write as hid_write
 
-def send_mouse_event(dev, buttons, x, y,
+def relative_mouse_event(dev, buttons, x, y,
                      vertical_wheel_delta, horizontal_wheel_delta, absolute = False):
     buf = [
         buttons,
+        x & 0xff,
+        y & 0xff,
         vertical_wheel_delta & 0xff,
         horizontal_wheel_delta & 0xff
     ]
+    hid_write.write_to_hid_interface(dev, buf)
 
-    if absolute:
-        buf[1:1] = [
-            x & 0xff,
-            (x >> 8) & 0xff,  # Extract the upper byte of x
-            y & 0xff,
-            (y >> 8) & 0xff,  # Extract the upper byte of y
-        ]
-    else:
-        buf[1:1] = [
-            x & 0xff,
-            y & 0xff
-        ]
 
+def absolute_mouse_event(dev, buttons, x, y,
+                     vertical_wheel_delta, horizontal_wheel_delta):
+    buf = [
+        buttons,
+        x & 0xff,
+        (x >> 8) & 0xff,  # Extract the upper byte of x
+        y & 0xff,
+        (y >> 8) & 0xff,  # Extract the upper byte of y
+        vertical_wheel_delta & 0xff,
+        horizontal_wheel_delta & 0xff
+    ]
     hid_write.write_to_hid_interface(dev, buf)
