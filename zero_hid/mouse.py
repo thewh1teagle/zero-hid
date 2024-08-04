458ca259ac4e3a44a1a32e94747492f5aa11ad7c
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-04 07:20:43 +0100

    add missing raise for exceptions

--- zero_hid/mouse.py
+++ zero_hid/mouse.py
@@ -94,14 +94,14 @@ class Mouse:
         if not -127 <= x <= 127:
             raise RelativeMoveRangeError(f"Value of x: {x} out of range (-127 - 127)")
         if not -127 <= y <= 127:
-            RelativeMoveRangeError(f"Value of y: {y} out of range (-127 - 127)")
+            raise RelativeMoveRangeError(f"Value of y: {y} out of range (-127 - 127)")
         self.__send_mouse_event(self.dev, self.buttons_state, x, y, 0, 0)
 
     def __move_absolute(self, x, y):
         if not 0 <= x <= 65535:
             raise RelativeMoveRangeError(f"Value of x: {x} out of range (0 - 65535)")
         if not 0 <= y <= 65535:
-            RelativeMoveRangeError(f"Value of y: {y} out of range (0 - 65535)")
+            raise RelativeMoveRangeError(f"Value of y: {y} out of range (0 - 65535)")
         self.__send_mouse_event(self.dev, self.buttons_state, x, y, 0, 0)
 
     def __enter__(self):
